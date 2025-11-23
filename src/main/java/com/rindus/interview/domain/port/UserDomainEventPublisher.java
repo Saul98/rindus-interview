fa41a135c8118ad57df97fa9791c2447ae8e264e
Saúl Cabeza <saul.cabeza@idneo.com>
2025-11-23 18:28:02 +0100

    feat: Add javadoc for event publisher port

--- src/main/java/com/rindus/interview/domain/port/UserDomainEventPublisher.java
+++ src/main/java/com/rindus/interview/domain/port/UserDomainEventPublisher.java
@@ -2,6 +2,9 @@ package com.rindus.interview.domain.port;
 
 import com.rindus.interview.domain.event.UserDomainEvent;
 
+/**
+ * Domain port for publishing UserDomainEvents.
+ */
 public interface UserDomainEventPublisher {
 
   void publish(UserDomainEvent event);
